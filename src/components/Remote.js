deff67cd38bc0515c7c21ec0334d0461602ed049
Iza-04 <Iza.m150492@gmail.com>
2025-12-26 19:55:07 +0500

    styles have been added, Remote.css has been added

--- src/components/Remote.js
+++ src/components/Remote.js
@@ -1,9 +1,9 @@
+import "./Remote.css";
+
 function Remote({
   isOn,
   setIsOn,
-  channel,
   changeChannel,
-  channelsCount,
   volume,
   setVolume,
   isMuted,
@@ -11,102 +11,30 @@ function Remote({
   showMenu,
   setShowMenu,
 }) {
-  const playSound = (src) => {
-    if (isMuted) return;
-    const audio = new Audio(src);
-    audio.volume = volume / 100;
-    audio.play().catch(() => {});
-  };
-
-  const click = () => playSound("/sounds/click.mp3");
-
-  const powerToggle = () => {
-    playSound(isOn ? "/sounds/power-off.mp3" : "/sounds/power-on.mp3");
-    setIsOn(!isOn);
-  };
-
-  const selectChannelByNumber = (number) => {
-    if (!isOn) return;
-    click();
-
-    const index = number - 1;
-    if (index < channelsCount) {
-      changeChannel(index);
-    }
-  };
-
   return (
     <div className="remote">
-      <h3>Пульт</h3>
-
-      <button className="power" onClick={powerToggle}>
-        ⏻
+      {/* КНОПКА ПИТАНИЯ */}
+      <button className="power" onClick={() => setIsOn(!isOn)}>
+        POWER
       </button>
 
-      <hr />
+      {/* КАНАЛЫ */}
+      <button onClick={() => changeChannel("up")}>CH +</button>
 
-      <button
-        onClick={() => {
-          click();
-          changeChannel((channel + 1) % channelsCount);
-        }}
-        disabled={!isOn}
-      >
-        Канал +
+      <button onClick={() => changeChannel("down")}>CH -</button>
+
+      {/* ГРОМКОСТЬ */}
+      <button onClick={() => setVolume(Math.min(volume + 10, 100))}>
+        VOL +
       </button>
 
-      <button
-        onClick={() => {
-          click();
-          changeChannel((channel - 1 + channelsCount) % channelsCount);
-        }}
-        disabled={!isOn}
-      >
-        Канал -
-      </button>
+      <button onClick={() => setVolume(Math.max(volume - 10, 0))}>VOL -</button>
 
-      <hr />
+      {/* MUTE */}
+      <button onClick={() => setIsMuted(!isMuted)}>MUTE</button>
 
-      {/* Цифровые кнопки */}
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(3, 1fr)",
-          gap: "5px",
-        }}
-      >
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((num) => (
-          <button
-            key={num}
-            onClick={() => selectChannelByNumber(num)}
-            disabled={!isOn}
-          >
-            {num}
-          </button>
-        ))}
-      </div>
-
-      <hr />
-
-      <button
-        onClick={() => {
-          click();
-          setIsMuted(!isMuted);
-        }}
-        disabled={!isOn}
-      >
-        {isMuted ? "🔇 Mute" : "🔊 Sound"}
-      </button>
-
-      <button
-        onClick={() => {
-          click();
-          setShowMenu(!showMenu);
-        }}
-        disabled={!isOn}
-      >
-        Меню
-      </button>
+      {/* MENU */}
+      <button onClick={() => setShowMenu(!showMenu)}>MENU</button>
     </div>
   );
 }
